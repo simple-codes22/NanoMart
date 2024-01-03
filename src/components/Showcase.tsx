28982e371dda773540c3e3e3c4aea8ed359896a7
Simeon Akanbi <simeonakanbi99@gmail.com>
2024-01-04 00:32:30 +0100

    Fixed issue on showcase reel component

--- src/components/Showcase.tsx
+++ src/components/Showcase.tsx
@@ -1,11 +1,13 @@
 import { Box } from "@chakra-ui/react";
-import firstImage from './../assets/Showcase/One.jpg';
-import secondImage from './../assets/Showcase/Four.jpg';
-// import thirdImage from './../assets/Showcase/Five.jpg';
-// import fourth from './../assets/Showcase/Two.jpg';
-import { useEffect, useState, useRef, useMemo } from "react";
-
-// import { Image } from "@chakra-ui/react";
+import firstImage from "./../assets/Showcase/One.jpg";
+import secondImage from "./../assets/Showcase/Four.jpg";
+import thirdImage from "./../assets/Showcase/Five.jpg";
+import fourth from "./../assets/Showcase/Two.jpg";
+import {
+  useEffect, 
+  useState, 
+  useRef, 
+  useMemo } from "react";
 
 interface showReel {
   title: string;
@@ -14,64 +16,81 @@ interface showReel {
 }
 
 const Showcase = () => {
-  const showcaseReel = useMemo(() => [
-    {
-      title: 'Image 1',
-      imageSrc: firstImage,
-      color: '#000',
-    },
-    {
-      title: 'Image 2',
-      imageSrc: secondImage,
-      color: '#ac8484'
-    },
-    // { 
-    //   title: 'Image 4',
-    //   imageSrc: fourth,
-    //   color: '#9f3e3e',
-    // },
-    // { 
-    //   title: 'Image 3',
-    //   imageSrc: thirdImage,
-    //   color: '#000',
-    // },
+  const showcaseReel = useMemo(
+    () => [
+      {
+        title: "Image 1",
+        imageSrc: firstImage,
+        color: "#000",
+      },
+      {
+        title: "Image 2",
+        imageSrc: secondImage,
+        color: "#ac8484",
+      },
+      {
+        title: "Image 3",
+        imageSrc: thirdImage,
+        color: "#9f3e3e",
+      },
+      {
+        title: "Image 4",
+        imageSrc: fourth,
+        color: "#000",
+      },
+    ],
+    []
+  );
 
-  ], []);
-  
   const [current, setCurrent] = useState<showReel>(showcaseReel[0])
   const counter = useRef(0);
-  // const checker = useRef(null);
+
 
   useEffect(()=> {
     console.log(showcaseReel.length);
-    setInterval(() => {
+    const interval = setInterval(() => {
       if (counter.current < showcaseReel.length) {
         setCurrent(showcaseReel[counter.current]);
+        // console.log(`Counter in the first condition: ${counter.current}`);
         counter.current++;
       } else {
         counter.current = 0;
-        setCurrent(showcaseReel[counter.current]);
+        // console.log("Counter is now reset to ", counter.current);
       }
     }, 5000)
+    return () => clearInterval(interval);
 
   }, [showcaseReel])
 
-
-
   return (
-    <Box maxWidth='1280px' width="100%" display='flex' justifyContent="center" alignItems='center'>
-      {current ? 
-        <Box width='100%' height='400px' display='flex' color={current.color} fontSize="30px" fontWeight='700' justifyContent='center' alignItems='center' sx={{
-              // background: `url(${current.imageSrc}) no-repeat cover`,
-              backgroundImage: `url(${current.imageSrc})`,
-              backgroundRepeat: 'no-repeat',
-              backgroundSize: 'cover',
-            }}>
-            {current.title}
-        </Box>
-        : <></>}
+    <Box
+      maxWidth="1280px"
+      width="100%"
+      display="flex"
+      justifyContent="center"
+      alignItems="center"
+      >
+      { current ?
+      <Box
+        width="100%"
+        height="400px"
+        display="flex"
+        color={current.color}
+        fontSize="30px"
+        fontWeight="700"
+        justifyContent="center"
+        alignItems="center"
+        sx={{
+          backgroundImage: `url(${current.imageSrc})`,
+          backgroundRepeat: "no-repeat",
+          backgroundSize: "cover",
+          backgroundPosition: 'Center'
+        }}
+      >
+        {current.title}
+      </Box> : <></>}
     </Box>
-  )
-}
+  );
+};
 
-export default Showcase
+export default Showcase;
